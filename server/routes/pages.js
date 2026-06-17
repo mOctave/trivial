9ff6a6dce21dd136aa36be28d48a21df53aa9f35
mOctave <mOctave31415@gmail.com>
2026-06-17 00:10:50 -0700

    Add further pagedisplay debugging

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -61,6 +61,7 @@ router.get("/user/:target", async (req, res) => {
 
 router.get("/deck/:target", async (req, res) => {
 	showpage("pages/deck", req, res);
+	console.log("[SHOWPAGE COMPLETE]");
 });
 
 router.get("/card/:target", async (req, res) => {
